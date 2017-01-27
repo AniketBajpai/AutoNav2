08f95e3863f6b74f8d39f8f8164d45818ebd6e16
quantumcoder <quantum.computing96@gmail.com>
2017-01-27 23:13:47 +0530

    Added undistortion

---
+++ experimental/undistort/src/undistort_test.cpp
@@ -0,0 +1,24 @@
+#include "undistort.h"
+#include "fishcam.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/types_c.h>
+
+using namespace cv;
+
+int main(int argc, char const *argv[]) {
+  std::string s = "/home/quantumcoder/Documents/car-project/experimental/undistort/src/calib_results_flycap.txt";
+  FishOcam f;
+  f.init(s);
+  std::cout << "Focal is " << f.focal << "\n";
+  std::cout << "Wout(from config file) is " << f.wout << "\n";
+  std::cout << "Hout is " << f.hout << "\n";
+  Size S = Size(f.wout, f.hout);
+  Mat image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+  Mat image2;
+  f.WarpImage(image, image2);   // undistort image
+
+  cv::imwrite(argv[2], image2);
+}
